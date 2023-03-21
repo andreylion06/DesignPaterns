ec2b186f479f93171fed5989c2db0de9960118b2
Andrii Sakhno <andreylion.zt@gmail.com>
2023-03-21 22:32:14 +0200

    Singleton

---
+++ Singleton/DatabaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    sealed public class DatabaseRepository
+    {
+        private DatabaseRepository() { }
+
+        public int Id { get; private set; }
+
+        private static DatabaseRepository? _instance;
+        private static readonly object _lock = new object();
+
+        public static DatabaseRepository Instance(int id)
+        {
+            if(_instance == null)
+            {
+                // Suspend other threads and do this code
+                lock (_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new DatabaseRepository();
+                        _instance.Id = id;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Singleton;
+
+Thread t1 = new Thread(() =>
+{
+    DatabaseRepository.Instance(1);
+});
+
+Thread t2 = new Thread(() =>
+{
+    DatabaseRepository.Instance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+Console.WriteLine(DatabaseRepository.Instance(0).Id);
+
+// Both threads are creating an instance with different Ids.
+// After that I join both threads. Id is equal to 1
+// So, I did not created two instances and Design Patern is
+// implemented correctly.
